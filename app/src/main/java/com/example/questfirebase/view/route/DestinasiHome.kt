90a7b3fa3ef250b6f689de15c2f0bb53f906d729
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2026-01-02 21:42:56 +0700

    mmenambahkan code DestinasiHome.kt

---
+++ app/src/main/java/com/example/questfirebase/view/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.questfirebase.view.route
+
+import com.example.questfirebase.R
+import com.example.questfirebase.view.route.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
