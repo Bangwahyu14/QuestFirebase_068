ff85ad39e7108aff43a2b1bf07243ddababd5dc0
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2026-01-08 18:22:51 +0700

    menambahkan code pada file PenyediaViewModel.kt

--- app/src/main/java/com/example/questfirebase/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/questfirebase/viewmodel/PenyediaViewModel.kt
@@ -1,10 +1,12 @@
 package com.example.questfirebase.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.questfirebase.repositori.AplikasiDataSiswa
+
 
 fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
         this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
@@ -14,5 +16,7 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiDataSiswa().container.repositorySiswa) }
         initializer { EntryViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+        initializer { DetailViewModel (this.createSavedStateHandle(), aplikasiDataSiswa().container.repositorySiswa) }
+        initializer { EditViewModel (this.createSavedStateHandle(), aplikasiDataSiswa().container.repositorySiswa) }
     }
 }
