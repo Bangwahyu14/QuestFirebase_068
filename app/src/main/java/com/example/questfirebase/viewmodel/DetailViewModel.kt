7fb998315d2f526d141ed3752e2b88e5dc562ec0
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2026-01-08 18:02:48 +0700

    menambahkan code statusuidetail di detailviewmodel

--- app/src/main/java/com/example/questfirebase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questfirebase/viewmodel/DetailViewModel.kt
@@ -1,4 +1,30 @@
+@file:OptIn(InternalSerializationApi::class)
 package com.example.questfirebase.viewmodel
 
-class DetailViewModel {
+import com.example.myfirebase.modeldata.Siswa
+import kotlinx.serialization.InternalSerializationApi
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.myfirebase.repositori.RepositorySiswa
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import com.example.myfirebase.view.route.DestinasiDetail
+import androidx.lifecycle.viewModelScope
+import com.example.questfirebase.modeldata.Siswa
+import com.example.questfirebase.view.route.DestinasiDetail
+import com.example.questfirebase.view.route.RepositorySiswa
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: Siswa?) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
+
+
+
+
+
 }
