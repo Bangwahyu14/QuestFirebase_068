94a6c808ed2acf354c348c91eefd1ec45f9c626f
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2026-01-08 18:04:52 +0700

    menambahkan code fun getstatusiswa di detailviewmodel

--- app/src/main/java/com/example/questfirebase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questfirebase/viewmodel/DetailViewModel.kt
@@ -38,6 +38,17 @@ class DetailViewModel(
         getSatuSiswa()
     }
 
-
+    fun getSatuSiswa() {
+        viewModelScope.launch {
+            statusUIDetail = StatusUIDetail.Loading
+            statusUIDetail = try {
+                StatusUIDetail.Success(satusiswa = repositorySiswa.getSatuSiswa(idSiswa))
+            } catch (e: IOException) {
+                StatusUIDetail.Error
+            } catch (e: Exception) {
+                StatusUIDetail.Error
+            }
+        }
+    }
 
 }
