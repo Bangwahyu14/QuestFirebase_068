8807271de6aa1cb0e880a9324ad424149d7232cb
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2026-01-08 18:08:19 +0700

    menambahkan code var uistatesiswa di EditViewModel.kt

--- app/src/main/java/com/example/questfirebase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questfirebase/viewmodel/EditViewModel.kt
@@ -1,4 +1,31 @@
 package com.example.questfirebase.viewmodel
 
-class EditViewModel {
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questfirebase.modeldata.DetailSiswa
+import com.example.questfirebase.modeldata.UIStateSiswa
+import com.example.questfirebase.modeldata.toSiswa
+import com.example.questfirebase.view.route.DestinasiDetail
+import com.example.questfirebase.view.route.RepositorySiswa
+import kotlinx.coroutines.launch
+
+class EditViewModel(savedStateHandle: SavedStateHandle, private val repositorySiswa:
+RepositorySiswa
+): ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+
+
+
+
+
+
+
+
 }
